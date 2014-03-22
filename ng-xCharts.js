b47fe3f9609af1aa6e4b8fd33acaa9a42405e105
Radu Cigmaian <radu.cigmaian@gmail.com>
2014-03-22 09:52:37 +0200

    id is not mandatory

--- ng-xCharts.js
+++ ng-xCharts.js
@@ -1,11 +1,20 @@
+
 angular.module('ng-xCharts', [])
-  .directive('xchart', function () {
-    return {
-      restrict: 'EACM',
-      template: '<figure></figure>',
-      replace: true,
-      link: function (scope, elem, attrs) {
-          new xChart(scope.$eval(attrs.type), scope.$eval(attrs.data), '#'+attrs.id, scope.$eval(attrs.opts));
-      }
-    };
-  });
+    .directive('xchart', function () {
+        var graphIdCount = 0;
+        return {
+            restrict: 'EACM',
+            template: '<figure></figure>',
+            replace: true,
+
+            link: function (scope, elem, attrs) {
+                var elemId;
+                if(!(elemId = elem.attr('id'))){
+                    graphIdCount++;
+                    elemId = 'graph-' + graphIdCount;
+                    elem.attr('id', elemId);
+                }
+                new xChart(scope.$eval(attrs.type), scope.$eval(attrs.data), '#' + elemId, scope.$eval(attrs.opts));
+            }
+        };
+    });
